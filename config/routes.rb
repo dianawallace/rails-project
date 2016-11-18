63784a8658bba95d752f5c4ad3e8d9b59a6295af
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-11-18 00:56:45 +0000

    customization of signin/login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  
   resources :users
+  
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  
   resources :products
   
   get 'static_pages/about'
