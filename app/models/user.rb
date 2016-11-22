c88615fd00982ace7fab7c6585ea40a0e5f3e899
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-11-22 00:36:29 +0000

    orders page need logged in access

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-  class User < ActiveRecord::Base
-  
     has_many :orders
   end
 
-end
