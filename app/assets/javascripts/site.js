d1832b14b880293d2f74c38436699ca2a001608f
Diana Wallace <dianawallacedeveloper@gmail.com>
2016-12-06 01:50:49 +0000

    javascript updated

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('ready page:load', function(){
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
