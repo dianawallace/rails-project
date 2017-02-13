e086f82ad69c4e90e2ce76a6fbc2c7d503f30463
Diana Wallace <dianawallacedeveloper@gmail.com>
2017-02-13 07:51:09 +0000

    6.6 post route

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
   
   get 'static_pages/landing_page'
   
+  post 'payments/create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
